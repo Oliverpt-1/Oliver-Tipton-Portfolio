27be49e893dda2f3b0c8bc8b7f5883b8032a7665
Oliverpt-1 <oliver@tiptonfarms.com>
2025-01-03 00:38:39 -0500

    fixed bug

--- Spear/bondingCurve.py
+++ Spear/bondingCurve.py
@@ -104,7 +104,6 @@ class BondingCurve(commands.Bot):
             events = self.event_filter.get_new_entries()
             for event in events:
                 if "0x" + event['topics'][0].hex() == self.graduated_event:
-                    await self.send_alert(event)
                     token_address = self.extract_token_address(event['data'])
                     print(f"[Graduated] Token: {token_address}")
                     await self.send_alert(f"[Graduated] Token: {token_address}")
