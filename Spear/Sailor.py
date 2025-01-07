c7d71fc2c0e6eadd34bee6f270bd1673875cb304
Oliverpt-1 <oliver@tiptonfarms.com>
2025-01-07 14:09:29 -0500

    moved debug

--- Spear/Sailor.py
+++ Spear/Sailor.py
@@ -117,7 +117,6 @@ class WhaleTracker(commands.Bot):
                     
                     # Check if transaction is from the last 5 minutes
                     if tx_time > current_time - datetime.timedelta(minutes=5):
-                        await self.send_alert(tx)
                         raw_amount = float(tx.get('amount', 0))
                         token_decimals = tx.get('token_decimals', 9)
                         token_address = tx.get('token_address', 'Unknown')
@@ -138,6 +137,7 @@ class WhaleTracker(commands.Bot):
 
 
                         if usd_value > 1000:
+                            await self.send_alert(tx)
                             recent_txs.append((actual_amount, usd_value, tx_time, token_address, token_name))
                         if wallet_name == "Him" or wallet_name == "Him 2":
                             if usd_value <= -1000:
